99011d3a83c829d4b12c3331e19c12ed56f7ca59
xqqy <xqqy189@hotmail.com>
2020-04-02 11:54:40 +0800

    Update myhighlighter.cpp

--- aweSomeEditorhiahia/src/myhighlighter.cpp
+++ aweSomeEditorhiahia/src/myhighlighter.cpp
@@ -1,10 +1,10 @@
 #include "myhighlighter.h"
 
 MyHighLighter::MyHighLighter(QTextDocument *parent)
-    : QSyntaxHighlighter(parent)
+    : QSyntaxHighlighter(parent)//重写了new时的方法
 {
     HighlightingRule rule;
-
+    //对于下列正则表达式，标为深蓝色
     keywordFormat.setForeground(Qt::darkBlue);
     keywordFormat.setFontWeight(QFont::Bold);
     QStringList keywordPatterns;
@@ -23,39 +23,46 @@ MyHighLighter::MyHighLighter(QTextDocument *parent)
         rule.format = keywordFormat;
         highlightingRules.append(rule);
     }
+    //对于下列正则表达式，标为紫色
     classFormat.setFontWeight(QFont::Bold);
     classFormat.setForeground(Qt::darkMagenta);
     rule.pattern = QRegExp("\\bQ[A-Za-z]+\\b");
     rule.format = classFormat;
     highlightingRules.append(rule);
 
+    //对于下列正则表达式，标为红色
     singleLineCommentFormat.setForeground(Qt::red);
     rule.pattern = QRegExp("//[^\n]*");
     rule.format = singleLineCommentFormat;
     highlightingRules.append(rule);
 
-    multiLineCommentFormat.setForeground(Qt::red);
 
+    //对于下列正则表达式，标为深绿色
     quotationFormat.setForeground(Qt::darkGreen);
     rule.pattern = QRegExp("\".*\"");
     rule.format = quotationFormat;
     highlightingRules.append(rule);
 
+    //对于下列正则表达式，标为斜体蓝色
     functionFormat.setFontItalic(true);
     functionFormat.setForeground(Qt::blue);
     rule.pattern = QRegExp("\\b[A-Za-z0-9_]+(?=\\()");
     rule.format = functionFormat;
     highlightingRules.append(rule);
 
+
+    //对于多行的标红，在highlightblock那块
+    multiLineCommentFormat.setForeground(Qt::red);
     commentStartExpression = QRegExp("/\\*");
-    commentEndExpression = QRegExp("\\*/");
+    commentEndExpression = QRegExp("\\*空格/");
 }
 
 void MyHighLighter::highlightBlock(const QString &text)
 {
+    //对于所有HighLightRule规则
     foreach (const HighlightingRule &rule, highlightingRules) {
         QRegExp expression(rule.pattern);
-        int index = expression.indexIn(text);
+        int index = expression.indexIn(text);//从头开始，到脚标上
         while (index >= 0) {
             int length = expression.matchedLength();
             setFormat(index, length, rule.format);
@@ -63,24 +70,26 @@ void MyHighLighter::highlightBlock(const QString &text)
         }
     }
 
+
+    //对于多行的标红
     setCurrentBlockState(0);
 
     int startIndex = 0;
     if (previousBlockState() != 1)
-        startIndex = commentStartExpression.indexIn(text);
+        startIndex = commentStartExpression.indexIn(text);//如果之前的语句没被标记过，找到第一个符合条件的多行注释语句并开始标红
 
 
-    while (startIndex >= 0) {
+    while (startIndex >= 0) {//找到所有开始标识符
         int endIndex = commentEndExpression.indexIn(text, startIndex);
         int commentLength;
-        if (endIndex == -1) {
-            setCurrentBlockState(1);
-            commentLength = text.length() - startIndex;
+        if (endIndex == -1) {//终止标识符
+            setCurrentBlockState(1);//表示这行搞定了
+            commentLength = text.length() - startIndex;//从头红到尾
         } else {
             commentLength = endIndex - startIndex
-                    + commentEndExpression.matchedLength();
+                    + commentEndExpression.matchedLength();//到终止标识符
         }
-        setFormat(startIndex, commentLength, multiLineCommentFormat);
-        startIndex = commentStartExpression.indexIn(text, startIndex + commentLength);
+        setFormat(startIndex, commentLength, multiLineCommentFormat);//设为红色
+        startIndex = commentStartExpression.indexIn(text, startIndex + commentLength);//继续找下一个有开始标识符的位置
     }
 }
